0d9ce125e3bb961e64e26d8746dbc214fac85926
winsukses351-oss <winsukses351@gmail.com>
2026-09-09 19:08:12 +0700

    Delete apps directory

--- apps/api/routes/__init__.py
+++
@@ -1,1 +0,0 @@
-# Routes Package Initialization
--- apps/api/web/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ["@solana-trader/shared"],
-  output: "standalone",
-}
-
-module.exports = nextConfig
